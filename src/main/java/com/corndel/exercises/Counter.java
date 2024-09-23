054c427c6a47010d07e0ce59b613ab58fb1bd54d
Damian <damiangildea12@gmail.com>
2024-09-23 11:00:53 +0100

    Exercise 2 done

--- src/main/java/com/corndel/exercises/Counter.java
+++ src/main/java/com/corndel/exercises/Counter.java
@@ -5,9 +5,19 @@ public class Counter {
     int count;
 
     public Counter() {
-
         this.count = 0;
+    }
 
+    public void increment() {
+        count++;
+    }
+
+    public void reset() {
+        count = 0;
+    }
+
+    public int getCount() {
+        return count;
     }
 
 
