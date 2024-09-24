41eb0f1eec9b5f549b788b6a52e288266fc5efb7
Damian <damiangildea12@gmail.com>
2024-09-24 14:54:16 +0100

    Rook and queen tests passing

--- src/main/java/com/corndel/chessington/model/pieces/Rook.java
+++ src/main/java/com/corndel/chessington/model/pieces/Rook.java
@@ -4,36 +4,76 @@ import com.corndel.chessington.model.Board;
 import com.corndel.chessington.model.Coordinates;
 import com.corndel.chessington.model.Move;
 import com.corndel.chessington.model.PlayerColour;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook implements Piece {
 
-  private final Piece.PieceType type;
-  protected final PlayerColour colour;
+    private final Piece.PieceType type;
+    protected final PlayerColour colour;
 
-  public Rook(PlayerColour colour) {
-    this.type = PieceType.ROOK;
-    this.colour = colour;
-  }
+    public Rook(PlayerColour colour) {
+        this.type = PieceType.ROOK;
+        this.colour = colour;
+    }
 
-  @Override
-  public Piece.PieceType getType() {
-    return type;
-  }
+    @Override
+    public Piece.PieceType getType() {
+        return type;
+    }
 
-  @Override
-  public PlayerColour getColour() {
-    return colour;
-  }
+    @Override
+    public PlayerColour getColour() {
+        return colour;
+    }
 
-  @Override
-  public String toString() {
-    return colour.toString() + " " + type.toString();
-  }
+    @Override
+    public String toString() {
+        return colour.toString() + " " + type.toString();
+    }
 
-  @Override
-  public List<Move> getAllowedMoves(Coordinates from, Board board) {
-    // TODO Implement this!
-    return List.of();
-  }
+    @Override
+    public List<Move> getAllowedMoves(Coordinates from, Board board) {
+        List<Move> allowedMoves = new ArrayList<>();
+
+        int[][] directions = {
+                {1, 0},
+                {0, -1},
+                {-1, 0},
+                {0, -1}
+        };
+
+        // Loop through each direction
+        for (int[] direction : directions) {
+            int rowOffset = direction[0];
+            int colOffset = direction[1];
+
+            int newRow = from.getRow() + rowOffset;
+            int newCol = from.getCol() + colOffset;
+
+            // Keep moving in the current direction until you hit the edge of the board or a piece
+            // If the square is empty the rook can keep moving until it hits a piece or the board edge
+            while (newRow >= 0 && newRow < Board.BOARD_SIZE && newCol >= 0 && newCol < Board.BOARD_SIZE) {
+                Piece targetPiece = board.get(new Coordinates(newRow, newCol));
+
+                if (targetPiece == null) {
+                    allowedMoves.add(new Move(from, new Coordinates(newRow, newCol)));
+                } else if (targetPiece.getColour() != this.colour) {
+                    allowedMoves.add(new Move(from, new Coordinates(newRow, newCol)));
+                    break;
+                }
+                // If there is a piece of the same color - stop
+                else {
+                    break;
+                }
+
+                // Continue moving in the same direction by updating coordinates
+                newRow += rowOffset;
+                newCol += colOffset;
+            }
+        }
+
+        return allowedMoves;
+    }
 }
