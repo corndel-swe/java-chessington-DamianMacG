4a3034345d821538e573c2ac882a3e34822be811
Damian <damiangildea12@gmail.com>
2024-09-24 14:27:16 +0100

    Day 2 exercises

--- src/main/java/com/corndel/exercises/Circle.java
+++ src/main/java/com/corndel/exercises/Circle.java
@@ -1,13 +1,23 @@
 package com.corndel.exercises;
 
-public class Circle {
-  private double radius;
+public class Circle implements Shape {
+    private double radius;
 
-  Circle(double radius) {
-    this.radius = radius;
-  }
+    Circle(double radius) {
+        this.radius = radius;
+    }
 
-  public double getRadius() {
-    return radius;
-  }
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
 }
