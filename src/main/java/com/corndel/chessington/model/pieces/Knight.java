450e323cb553aabe7276588b057c834996cf74ee
Damian <damiangildea12@gmail.com>
2024-09-26 15:02:16 +0100

    Knight refactored extending abtract class and associated class updates

--- src/main/java/com/corndel/chessington/model/pieces/Knight.java
+++ src/main/java/com/corndel/chessington/model/pieces/Knight.java
@@ -8,29 +8,15 @@ import com.corndel.chessington.model.PlayerColour;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Knight implements Piece {
-
-    private final Piece.PieceType type;
-    protected final PlayerColour colour;
+public class Knight extends AbstractPiece {
 
     public Knight(PlayerColour colour) {
-        this.type = PieceType.KNIGHT;
-        this.colour = colour;
-    }
-
-    @Override
-    public Piece.PieceType getType() {
-        return type;
-    }
-
-    @Override
-    public PlayerColour getColour() {
-        return colour;
+        super(PieceType.KNIGHT, colour);
     }
 
     @Override
     public String toString() {
-        return colour.toString() + " " + type.toString();
+        return getColour() + " " + getType();
     }
 
     @Override
